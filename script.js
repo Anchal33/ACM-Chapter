6330df33454d84fc846ff3de73e648f8158821ea
TanishqSingla <tanishqsingla00@gmail.com>
2020-08-15 18:47:26 +0530

    Centering footer elements and eliminated niche warnings and error
    Centered the elements of footer
    changed the location for scripts eliminating niche warnings caused by cdns

--- script.js
+++ script.js
@@ -1,51 +1,48 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  /* For the sticky navigation */
+  $(".section-about").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    },
+    {
+      offset: "60px;",
+    }
+  );
 
+  //Mobile Responsive
+  $(".mobile-nav-icon").click(function () {
+    var nav = $(".js-main-nav");
+    var icon = $(".mobile-nav-icon i");
 
-    /* For the sticky navigation */
-    $('.section-about').waypoint(function(direction) {
-        if (direction == "down") {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
-        }
-    }, {
-      offset: '60px;'
-    });
-
-             //Mobile Responsive
-    $('.mobile-nav-icon').click(function() {
-        var nav = $('.js-main-nav');
-        var icon = $('.mobile-nav-icon i');
-
-
-        if (icon.hasClass('ion-navicon-round')) {
-            icon.addClass('ion-close-round');
-            icon.removeClass('ion-navicon-round');
-        } else {
-            icon.addClass('ion-navicon-round');
-            icon.removeClass('ion-close-round');
-        }
-        nav.slideToggle(200);
-
-    });
-
- $('.past').click(function(){
-    $('.upCarousel div:first').removeClass('show');
-   $('.pastCarousel div:first').removeClass('hide');
-   $('.pastCarousel div:first').addClass('show');
-$('.upCarousel div:first').addClass('hide');
-$('.past h3').addClass('borderdown');
-$('.up h3').removeClass('borderdown');
- })
-
- $('.up').click(function(){
-    $('.pastCarousel div:first').removeClass('show');
-   $('.pastCarousel div:first').addClass('hide');
-   $('.upCarousel div:first').removeClass('hide');
-   $('.upCarousel div:first').addClass('show');
-$('.up h3').addClass('borderdown');
-$('.past h3').removeClass('borderdown');
-
- })
-
+    if (icon.hasClass("ion-navicon-round")) {
+      icon.addClass("ion-close-round");
+      icon.removeClass("ion-navicon-round");
+    } else {
+      icon.addClass("ion-navicon-round");
+      icon.removeClass("ion-close-round");
+    }
+    nav.slideToggle(200);
   });
+
+  $(".past").click(function () {
+    $(".upCarousel div:first").removeClass("show");
+    $(".pastCarousel div:first").removeClass("hide");
+    $(".pastCarousel div:first").addClass("show");
+    $(".upCarousel div:first").addClass("hide");
+    $(".past h3").addClass("borderdown");
+    $(".up h3").removeClass("borderdown");
+  });
+
+  $(".up").click(function () {
+    $(".pastCarousel div:first").removeClass("show");
+    $(".pastCarousel div:first").addClass("hide");
+    $(".upCarousel div:first").removeClass("hide");
+    $(".upCarousel div:first").addClass("show");
+    $(".up h3").addClass("borderdown");
+    $(".past h3").removeClass("borderdown");
+  });
+});
